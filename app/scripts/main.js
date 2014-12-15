eb8889eba2a74ca6e03f6295f3f56688e938a4ab
aneeshmg <aneesh.mg2007@gmail.com>
2014-12-15 21:06:17 +0530

    added queryloader2 and setup its init

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,15 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function(){
+  console.log("loaded...");
+
+  // QueryLoader2 - site preloader configuration
+  $("body").queryLoader2({
+    fadeOutTime: 300,
+    minimumTime: 500,
+    barColor: '#efefef',
+    backgroundColor: '#000',
+    percentage: true,
+    barHeight: 1
+  });
+
+
+});
